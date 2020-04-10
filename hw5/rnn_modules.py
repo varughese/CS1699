2ace933a17bc2bff16ef0f57ae2b4f271a3287cf
Mathew Varughese <mat.varughese3@gmail.com>
2020-04-09 21:41:59 -0400

    peephole lstm

--- hw5/rnn_modules.py
+++ hw5/rnn_modules.py
@@ -1,6 +1,6 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 
 
@@ -120,7 +120,7 @@ class LSTMCell(nn.Module):
       prev_c = prev_state[1]
 
     # We can stack prev_h and x on top of each other, and apply matrix multiplies to them
-    concat_hx = torch.cat((prev_h, x), dim=1);
+    concat_hx = torch.cat((prev_h, x), dim=1)
 
     # Formulas are from https://colah.github.io/posts/2015-08-Understanding-LSTMs/
     # The only way to understand what this actually does is read that
@@ -157,21 +157,62 @@ class PeepholedLSTMCell(nn.Module):
 
     self.input_size = input_size
     self.hidden_size = hidden_size
+    # cell state size is the same as hidden, and in peephole we
+    # stack prev cell state on the prev hidden state and input,
+    # which changes the size of our network
+    cell_state_size = hidden_size 
     self.bias = bias
 
-    #####################################################################
-    # Implement here following the given signature                      #
-    raise NotImplementedError
-    #####################################################################
+    self.W_f = nn.Parameter(torch.Tensor(hidden_size, cell_state_size + hidden_size + input_size))
+    if bias:
+      self.b_f = nn.Parameter(torch.Tensor(hidden_size))
+    else:
+      self.register_parameter('b_f', None)
+
+    self.W_i = nn.Parameter(torch.Tensor(hidden_size, cell_state_size + hidden_size + input_size))
+    if bias:
+      self.b_i = nn.Parameter(torch.Tensor(hidden_size))
+    else:
+      self.register_parameter('b_i', None)
+
+    self.W_o = nn.Parameter(torch.Tensor(hidden_size, cell_state_size + hidden_size + input_size))
+    if bias:
+      self.b_o = nn.Parameter(torch.Tensor(hidden_size))
+    else:
+      self.register_parameter('b_o', None)
+
+    self.W_c = nn.Parameter(torch.Tensor(hidden_size, hidden_size + input_size))
+    if bias:
+      self.b_c = nn.Parameter(torch.Tensor(hidden_size))
+    else:
+      self.register_parameter('b_c', None)
 
     return
 
   def forward(self, x, prev_state):
-    #####################################################################
-    # Implement here following the given signature                      #
-    raise NotImplementedError
-    #####################################################################
-    return
+    if prev_state is None:
+      batch = x.shape[0]
+      prev_h = torch.zeros((batch, self.hidden_size), device=x.device)
+      prev_c = torch.zeros((batch, self.hidden_size), device=x.device)
+    else:
+      prev_h = prev_state[0]
+      prev_c = prev_state[1]
+
+    # We can stack prev_h and x on top of each other, and apply matrix multiplies to them
+    concat_hx = torch.cat((prev_h, x), dim=1)
+    # The "peephole" is that all gates have access to C
+    concat_chx = torch.cat((prev_c, prev_h, x), dim=1)
+
+    # Formulas are from https://colah.github.io/posts/2015-08-Understanding-LSTMs/
+    # The only way to understand what this actually does is read that
+    f_t = torch.sigmoid(F.linear(concat_chx, self.W_f, self.b_f))
+    i_t = torch.sigmoid(F.linear(concat_chx, self.W_i, self.b_i))
+    C_tilde = torch.tanh(F.linear(concat_hx, self.W_c, self.b_c))
+    o_t = torch.sigmoid(F.linear(concat_chx, self.W_o, self.b_o))
+    
+    C = f_t * prev_c + i_t * C_tilde # next cell state
+    h = o_t * torch.tanh(C)
+    return h, C
 
   def reset_parameters(self):
     sqrt_k = (1. / self.hidden_size)**0.5
